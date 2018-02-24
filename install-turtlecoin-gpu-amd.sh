72e94e4fa0eaf973c41c9d140436fd99ab525582
Cat5TV <contact@category5.tv>
2018-02-24 12:47:33 -0500

    Add TRTL AMD

---
+++ install-turtlecoin-gpu-amd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Mine Turtlecoin in support of the Category5 TV Network
+# Family-friendly broadcasting from Canada
+# See https://category5.tv/
+
+# This script installs Turtlecoin GPU mining (AMD) on a Debian-based Linux system
+
+if [[ $EUID -ne 0 ]]; then
+  echo "ERROR: This script must be run as root" 2>&1
+  exit 1
+else
+
+dest="/usr/local/share/cat5tv-miners" # No trailing slash
+wallet=$(wget -qO- https://raw.githubusercontent.com/Cat5TV/cat5tv-miners/master/wallets/cat5tv-turtlecoin)
+
+name=$(basename $0)
+name="${name%.*}"
+name=${name#install-}
+
+# Install dependencies
+apt update
+apt -y install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libz-dev git build-essential cmake libuv1-dev libmicrohttpd-dev opencl-headers ocl-icd-opencl-dev
+
+# Build XMRIG
+mkdir -p $dest/software && cd $dest/software && git clone https://github.com/xmrig/xmrig-amd && mv xmrig-amd $name && cd $name && cmake . && make
+
+# Create the bash script to execute Monero mining to Category5's wallet
+echo "#!/bin/bash" > $dest/$name.sh
+echo "port=3333 # For SBC/Up to 100 H/s" >> $dest/$name.sh
+echo "$dest/software/$name/xmrig-amd -o turtle.mining.garden:\$port -u $wallet -p cat5tv -k --donate-level=1" >> $dest/$name.sh
+chmod +x $dest/$name.sh
+
+echo Done. To begin mining Monero type: $dest/$name.sh
+
+fi
