8e7162ad6e17ef1bb58de354b415064c1984e0d2
Robbie Ferguson <contact@category5.tv>
2018-02-26 14:46:04 -0500

    Auto ready

--- automine-cpu.sh
+++ automine-cpu.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-timer=24; # how many hours before query and restart
+timer=1; # how many hours before cycling currencies
+
+currencies="monero,turtlecoin" # CSV
+
+# Choose a random currency to begin with...
+random=( ${currencies//,/ } )
+num=$(( ${#random[@]} -1 ))
+newseq=$( shuf -i 0-$num )
+options=()
+for i in $newseq; do
+     options+=("${random[$i]}")
+done
 
 while :
 do
-  let time=timer*3600
-  conf=$(wget -qO- https://raw.githubusercontent.com/Cat5TV/cat5tv-miners/master/automine.conf)
-  executable=/usr/local/share/cat5tv-miners/$conf-cpu.sh
-  if [[ ! -f $executable ]]; then
-    echo You are missing the miner for $conf. Please run: sudo ./install-$conf-cpu.sh
-    sleep 3600
-  fi
-  $executable& sleep $time; kill $!
-
+  for option in "${options[@]}"
+  do
+    let time=timer*3600
+    executable=/usr/local/share/cat5tv-miners/$option-cpu.sh
+    if [[ ! -f $executable ]]; then
+      echo You are missing the miner for $option. Please run: sudo ./install-$option-cpu.sh
+      sleep 3600
+    fi
+    $executable& sleep $time; kill $!
+  done
 done
+
