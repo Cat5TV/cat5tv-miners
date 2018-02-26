a20b795b0bc9db9dd9fc7072a08e33dcc697363e
Robbie Ferguson <contact@category5.tv>
2018-02-26 11:47:27 -0500

    Test

--- automine-cpu.sh
+++ automine-cpu.sh
@@ -5,7 +5,11 @@ while :
 do
   let time=timer*3600
   conf=$(wget -qO- https://raw.githubusercontent.com/Cat5TV/cat5tv-miners/master/automine.conf)
-
-  timeout $time /usr/local/share/cat5tv-miners/$conf-cpu.sh
+  executable=/usr/local/share/cat5tv-miners/$conf-cpu.sh
+  if [[ ! -f $executable ]]; then
+    echo You are missing the miner for $conf. Please run: sudo ./install-$conf-cpu.sh
+    sleep 3600
+  fi
+  $executable& sleep $time; kill $!
 
 done
