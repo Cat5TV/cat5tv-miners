c182f2e05194947c61cc684d0cd287f88449075c
Robbie Ferguson <contact@category5.tv>
2018-03-09 12:16:42 -0500

    mods

--- windows/src/cat5tv-miners-cpu-autominer/automine.bat
+++ windows/src/cat5tv-miners-cpu-autominer/automine.bat
@@ -1,17 +1,33 @@
 @echo off
+
 echo Press CTRL-C two or three times to close.
+
 echo Press any key to switch to the next currency.
+
 echo ""
+
 timeout /t 5 /nobreak > NUL
 
+
+
 :loop
- set config=monero.cfg
+
+
+  set config=monero.cfg
+
   start xmrig.exe %config%
   timeout /t 10
+
   taskkill /im xmrig.exe /f
- set config=turtlecoin.cfg
+
+
+  set config=turtlecoin.cfg
+
   start xmrig.exe %config%
   timeout /t 10
+
   taskkill /im xmrig.exe /f
+
+
 goto loop
 
