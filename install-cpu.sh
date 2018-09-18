d46bd0527b056142a0ff2d64d6bbcba813a4d970
Robbie Ferguson <contact@category5.tv>
2018-09-18 09:58:55 -0400

    Add BTC

--- install-cpu.sh
+++ install-cpu.sh
@@ -30,7 +30,7 @@ else
   wget -O /tmp/install-stellite-cpu.sh https://raw.githubusercontent.com/Cat5TV/cat5tv-miners/master/install-stellite-cpu.sh && chmod +x /tmp/install-stellite-cpu.sh && sudo /tmp/install-stellite-cpu.sh
 
   # Install Bitcoin CPU miner
-#  wget -O /tmp/install-bitcoin-cpu.sh https://raw.githubusercontent.com/Cat5TV/cat5tv-miners/master/install-bitcoin-cpu.sh && chmod +x /tmp/install-bitcoin-cpu.sh && sudo /tmp/install-bitcoin-cpu.sh
+  wget -O /tmp/install-bitcoin-cpu.sh https://raw.githubusercontent.com/Cat5TV/cat5tv-miners/master/install-bitcoin-cpu.sh && chmod +x /tmp/install-bitcoin-cpu.sh && sudo /tmp/install-bitcoin-cpu.sh
 
   wget -O /usr/local/share/cat5tv-miners/automine-cpu.sh https://raw.githubusercontent.com/Cat5TV/cat5tv-miners/master/automine-cpu.sh && chmod +x /usr/local/share/cat5tv-miners/automine-cpu.sh
   wget -O /usr/local/share/cat5tv-miners/stopmine.sh https://raw.githubusercontent.com/Cat5TV/cat5tv-miners/master/stopmine.sh && chmod +x /usr/local/share/cat5tv-miners/stopmine.sh
